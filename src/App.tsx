3fb772fe61b7755a5c235311f8d5e5dedf807fea
Quang Minh <102916938+QuangMinh45@users.noreply.github.com>
2022-05-19 10:35:54 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import React from "react";
+import "./App.css";
+import Button from "./components/common/Button";
+import Pagination from "./components/common/Pagination";
+
+const App = () => {
+  const { Header, Sider, Content } = Layout;
+
+  return (
+    <Layout>
+      <Sider>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{ padding: 0 }}
+        ></Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <Button type="primary">Hủy</Button>
+          <Button>Lưu</Button>
+          <Pagination />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
